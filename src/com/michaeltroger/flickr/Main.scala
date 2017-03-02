c367e3e4cdeaf1bb2b4068f80e9918cbd4406e46
Michael Troger <michaeltroger@users.noreply.github.com>
2017-03-02 11:17:01 +0100

    refactored main

--- src/com/michaeltroger/flickr/Main.scala
+++ src/com/michaeltroger/flickr/Main.scala
@@ -6,54 +6,14 @@ import scala.swing._
 import scala.swing.event._
 
 object SwingApp extends SimpleSwingApplication  {
-
-  val searchField = new TextField {
-    listenTo(keys)
-    columns = 10
-  }
-
-  val searchButton = new Button {
-    text = "Search"
-  }
-
-  val recentImagesButton = new Button {
-    text = "Recent images"
-  }
-
-  val menuPanel = new FlowPanel{
-    contents.append(searchField, searchButton, recentImagesButton)
-    val s = new Dimension(800,10)
-    maximumSize = s
-  }
-
-  val imagePanel = new FlowPanel {
-    for (i <- 1 to 10) {
-      contents.append(new Label{
-          listenTo(mouse.clicks)
-          reactions += {case e : MouseClicked => openWebPage(e.source.tooltip)}
-      })
-    }
-  }
+  val menuPanel = createMenuPanel()
+  val imagePanel = createImagePanel()
 
   val recentImages = RecentImages(imagePanel)
   val searchImages = SearchImages(imagePanel)
   recentImages.getImageUrls()
 
-  recentImagesButton.reactions += {
-    case b : ButtonClicked => recentImages.getImageUrls()
-  }
-  searchButton.reactions += {
-    case b : ButtonClicked =>
-      searchImages.getImageUrls(("text", searchField.text))
-      searchField.text = ""
-  }
-  searchField.reactions += {
-    case KeyPressed(_, Key.Enter, _, _) =>
-      searchImages.getImageUrls(("text", searchField.text))
-      searchField.text = ""
-  }
-
-  val myPanel = new BoxPanel(Orientation.Vertical) {
+  val mainPanel = new BoxPanel(Orientation.Vertical) {
     contents.append(menuPanel, imagePanel)
   }
 
@@ -64,11 +24,61 @@ object SwingApp extends SimpleSwingApplication  {
     minimumSize = s
     preferredSize = s
 
-    contents = myPanel
+    contents = mainPanel
   }
 
   def openWebPage(url: String): Unit = {
     Desktop.getDesktop.browse(new URL(url).toURI)
+  }
+
+  def createMenuPanel(): FlowPanel = {
+    val searchField = new TextField {
+      listenTo(keys)
+      columns = 10
+    }
+
+    val searchButton = new Button {
+      text = "Search"
+    }
+
+    val recentImagesButton = new Button {
+      text = "Recent images"
+    }
+
+    val menuPanel = new FlowPanel{
+      contents.append(searchField, searchButton, recentImagesButton)
+      val s = new Dimension(800,10)
+      maximumSize = s
+    }
+
+    recentImagesButton.reactions += {
+      case b : ButtonClicked => recentImages.getImageUrls()
+    }
+    searchButton.reactions += {
+      case b : ButtonClicked =>
+        searchImages.getImageUrls(("text", searchField.text))
+        searchField.text = ""
+    }
+    searchField.reactions += {
+      case KeyPressed(_, Key.Enter, _, _) =>
+        searchImages.getImageUrls(("text", searchField.text))
+        searchField.text = ""
+    }
+
+    menuPanel
+  }
+
+  def createImagePanel(): FlowPanel = {
+    val imagePanel = new FlowPanel {
+      for (i <- 1 to 10) {
+        contents.append(new Label{
+          listenTo(mouse.clicks)
+          reactions += {case e : MouseClicked => openWebPage(e.source.tooltip)}
+        })
+      }
+    }
+
+    imagePanel
   }
 }
 
